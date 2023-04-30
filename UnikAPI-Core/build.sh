81babe7bdd04eb3f6c53c055d6cb8f0a0c0570e0
_Mads <75088349+TFSMads@users.noreply.github.com>
2023-04-30 21:12:34 +0200

    Update build.sh

--- UnikAPI-Core/build.sh
+++ UnikAPI-Core/build.sh
@@ -3,5 +3,6 @@ chmod +x ./gradlew
 ./gradlew build -Dorg.gradle.java.home=$JAVA_8
 
 cp build/libs/UnikAPI-Core-1.0-all.jar ../merge/UnikAPI-Core.jar
+mkdir ../UnikApi-Laby4/core/libs/
 cp build/libs/UnikAPI-Core-1.0-all.jar ../UnikApi-Laby4/core/libs/UnikAPI-Core.jar
 
