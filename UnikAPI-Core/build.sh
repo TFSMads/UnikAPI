cddc03c8370411817b09eb3a88de088b883cfec9
_Mads <75088349+TFSMads@users.noreply.github.com>
2023-04-30 20:37:05 +0200

    Update build.sh

--- UnikAPI-Core/build.sh
+++ UnikAPI-Core/build.sh
@@ -1,5 +1,5 @@
 chmod +x ./gradlew
-./gradlew clean
-./gradlew build
+./gradlew clean -Dorg.gradle.java.home=$JAVA_HOME_8_x64
+./gradlew build -Dorg.gradle.java.home=$JAVA_HOME_8_x64
 
 cp build/libs/UnikAPI-Core-1.0-all.jar ../merge/UnikAPI-Core.jar
