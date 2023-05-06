03fa41b77410142d9a5487fe6cd0d391c99b240d
_Mads <75088349+TFSMads@users.noreply.github.com>
2023-05-06 18:56:57 +0200

    Update build.sh

--- UnikApi-Laby4/build.sh
+++ UnikApi-Laby4/build.sh
@@ -1,5 +1,5 @@
 chmod +x ./gradlew
-./gradlew clean
-./gradlew build
+./gradlew clean -Dorg.gradle.java.home="$JAVA_17"
+./gradlew build -Dorg.gradle.java.home="$JAVA_17"
 
 cp build/libs/UnikAPI-Laby4-release.jar ../merge/UnikAPI-Laby4.jar
