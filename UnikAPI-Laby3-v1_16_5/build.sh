75b291b498d9e21d76d85f3d6563f47f9b09da6f
_Mads <75088349+TFSMads@users.noreply.github.com>
2023-06-25 17:49:41 +0200

    Update build.sh

--- UnikAPI-Laby3-v1_16_5/build.sh
+++ UnikAPI-Laby3-v1_16_5/build.sh
@@ -1,5 +1,5 @@
 chmod +x ./gradlew
-./gradlew clean
-./gradlew build
+./gradlew clean -Dorg.gradle.java.home="$JAVA_8"
+./gradlew build -Dorg.gradle.java.home="$JAVA_8"
 
 cp build/libs/UnikAPI-1.0.0.jar ../merge/UnikAPI-Laby3-v1_16_5.jar
