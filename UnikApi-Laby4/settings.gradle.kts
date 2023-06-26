33c9d5bab42f62f04dc91442639dad50348f0eb8
_Mads <madsvolder@gmail.com>
2023-06-26 14:14:36 +0200

    Update gradle plugin for labymod 4

--- UnikApi-Laby4/settings.gradle.kts
+++ UnikApi-Laby4/settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "UnikAPI-Laby4"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.22"
+    val labyGradlePluginVersion = "0.3.26"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
